12993f75108e99b85f6e4e014e3eb717a46afaef
Carlton Joseph <carlton.joseph@gmail.com>
2022-07-13 17:27:55 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense } from "react";
+import { createMachine } from "xstate";
+import { inspect } from "@xstate/inspect";
+import { atomWithMachine } from "jotai/xstate";
+import { useAtom, atom, Provider } from "jotai";
 
+import "./style.css";
+
+inspect();
+
+const lightMachine = createMachine({
+  id: "lightMachine",
+  initial: "green",
+  states: {
+    green: { on: { NEXT: "yellow" } },
+    yellow: { on: { NEXT: "red" } },
+    red: { on: { NEXT: "green" } },
+  },
+});
+
+const lightAtom = atomWithMachine(lightMachine, { devTools: true });
+
+const fetchLightMessage = async (light) => {
+  await new Promise((r) => setTimeout(r, 1500));
+  return `Hello ${light}`;
+};
+
+const asyncMessage = atom(async (get) => {
+  const light = get(lightAtom).value;
+  const message = await fetchLightMessage(light);
+  return message;
+});
+
+const LightMessage = () => {
+  const [message] = useAtom(asyncMessage);
+  return (
+    <text x={100} y={20}>
+      {message}
+    </text>
+  );
+};
+
+const Loader = ({ y = 15 }) => (
+  <g>
+    <circle fill="gray" cx={110} cy={y}>
+      <animate
+        attributeName="r"
+        values="3;8;4;6;3"
+        dur="800ms"
+        repeatCount="indefinite"
+      />
+    </circle>
+    <text x={120} y={y + 5}>
+      Loading...
+    </text>
+  </g>
+);
+
+const Light = () => {
+  const [state, send] = useAtom(lightAtom);
+
+  const handleClick = () => {
+    send({ type: "NEXT" });
+  };
+  return (
+    <svg width={200} height={100}>
+      <circle fill={state.value} onClick={handleClick} r={40} cx={40} cy={40} />
+      <Suspense fallback={<Loader />}>
+        <LightMessage />
+      </Suspense>
+    </svg>
+  );
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Provider>
+          <Light />
+          <Light />
+        </Provider>
+      </div>
+      <div>
+        <Provider>
+          <Light />
+          <Light />
+        </Provider>
+      </div>
+    </>
   );
 }
 
